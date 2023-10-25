31aa4fc4d13fc7222fd74679062b1c0ed9efb7b1
Reginaldo Filho <reginaldo.fillo@gmail.com>
2023-10-24 21:21:02 -0300

    DTOs ChildTask e Objective

---
+++ codebusters/src/main/java/com/codebusters/codebusters/models/dtos/ObjectiveDTO.java
@@ -0,0 +1,63 @@
+package com.codebusters.codebusters.models.dtos;
+
+import com.codebusters.codebusters.models.entities.ChildUser;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
+public class ObjectiveDTO {
+    private Long id;
+
+    @NotBlank(message = "O campo código não pode estar em branco")
+    @NotEmpty(message = "O campo código não pode estar vazio")
+    private double objectiveValue;
+
+    private double currentAmount;
+
+    @NotBlank(message = "O campo código não pode estar em branco")
+    @NotEmpty(message = "O campo código não pode estar vazio")
+    private String description;
+
+    private ChildUser childUserDTO;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getObjectiveValue() {
+        return objectiveValue;
+    }
+
+    public void setObjectiveValue(double objectiveValue) {
+        this.objectiveValue = objectiveValue;
+    }
+
+    public double getCurrentAmount() {
+        return currentAmount;
+    }
+
+    public void setCurrentAmount(double currentAmount) {
+        this.currentAmount = currentAmount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ChildUser getChildUserDTO() {
+        return childUserDTO;
+    }
+
+    public void setChildUserDTO(ChildUser childUserDTO) {
+        this.childUserDTO = childUserDTO;
+    }
+}
