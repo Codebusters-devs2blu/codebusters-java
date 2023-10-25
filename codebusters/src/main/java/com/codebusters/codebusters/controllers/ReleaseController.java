406f544414da7ca93c289cbf706d954c47646018
Eliézer da Silva <eliezerdasilva2005@gmail.com>
2023-10-25 19:58:30 -0300

    implementation of standard methods

--- codebusters/src/main/java/com/codebusters/codebusters/controllers/ReleaseController.java
+++ codebusters/src/main/java/com/codebusters/codebusters/controllers/ReleaseController.java
@@ -1,17 +1,56 @@
 package com.codebusters.codebusters.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codebusters.codebusters.interfaces.CrudController;
+import com.codebusters.codebusters.models.dtos.ReleaseDTO;
+import com.codebusters.codebusters.models.dtos.WalletDTO;
 import com.codebusters.codebusters.services.ReleaseService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/api/release")
-public class ReleaseController {
+public class ReleaseController  implements CrudController<ReleaseDTO, Long> {
 
 	@Autowired
-	private ReleaseService releaseService; 
+	private ReleaseService releaseService;
+
+	@Override
+	public List<ReleaseDTO> listAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<ReleaseDTO> findById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<Object> create(@Valid ReleaseDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<ReleaseDTO> update(@Valid ReleaseDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<Object> deleteById(Long id) {
+		return null;
+		// TODO Auto-generated method stub
+		
+	} 
 	
 	
 }
--- codebusters/src/main/java/com/codebusters/codebusters/controllers/WalletController.java
+++ codebusters/src/main/java/com/codebusters/codebusters/controllers/WalletController.java
@@ -1,15 +1,55 @@
 package com.codebusters.codebusters.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codebusters.codebusters.interfaces.CrudController;
+import com.codebusters.codebusters.models.dtos.ChildTaskDTO;
+import com.codebusters.codebusters.models.dtos.WalletDTO;
 import com.codebusters.codebusters.services.WalletService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/api/wallet")
-public class WalletController {
+public class WalletController implements CrudController<WalletDTO, Long> {
 
 	@Autowired
 	private WalletService walletService;
+
+	@Override
+	public List<WalletDTO> listAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<WalletDTO> findById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<Object> create(@Valid WalletDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<WalletDTO> update(@Valid WalletDTO dto) {
+		// TODO Auto-generated method stub
+		
+		return null;
+	}
+
+	@Override
+	public ResponseEntity<Object> deleteById(Long id) {
+		return null;
+		// TODO Auto-generated method stub
+		
+	}
 }
